96c88a0066ee2c3d10398fd6293231d7602e37b5
andre <a.ekani@gmail.com>
2024-09-30 12:44:42 +0200

    Add react rc

--- src/use-previous.ts
+++ src/use-previous.ts
@@ -2,7 +2,7 @@
 import { useRef, useEffect } from 'react';
 
 const usePrevious = <T>(value: T): T | undefined => {
-  const ref = useRef<T>();
+  const ref = useRef<T>(undefined);
 
   useEffect(() => {
     ref.current = value;
