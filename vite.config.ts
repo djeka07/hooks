ed4110af8e70007d6be405226709800a9899d905
andre <a.ekani@gmail.com>
2024-10-01 13:28:46 +0200

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import dts from 'vite-plugin-dts';
+import { resolve } from 'path';
 import preserveDirectives from 'rollup-preserve-directives';
-import { resolve, join, relative, dirname } from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
@@ -14,24 +14,15 @@ export default defineConfig({
   ],
   build: {
     copyPublicDir: false,
-    emptyOutDir: true,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      formats: ['es'],
+      fileName: (format, entryName) => `${entryName}.${format === 'es' ? 'js' : format}`,
+      formats: ['es', 'cjs'],
     },
     rollupOptions: {
       external: ['react', 'react-dom', 'react/jsx-runtime', 'framer-motion', 'polished'],
       output: {
         preserveModules: true,
-        entryFileNames: (entry) => {
-          const { name, facadeModuleId } = entry;
-          const fileName = `${name}.js`;
-          if (!facadeModuleId) {
-            return fileName;
-          }
-          const relativeDir = relative(resolve(__dirname, 'src'), dirname(facadeModuleId));
-          return join(relativeDir, fileName);
-        },
         globals: {
           react: 'React',
           'react/jsx-runtime': 'react/jsx-runtime',
