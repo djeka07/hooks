ec63fc212cfe97c2bc7126ed90dd5c475c8c0504
Andre Ekbom <andreekbom@live.se>
2024-12-06 20:57:35 +0100

    Update packages and add utils to external

--- vite.config.ts
+++ vite.config.ts
@@ -30,12 +30,13 @@ export default defineConfig({
       fileName: (format, entryName) => `${entryName}.${format === 'es' ? 'mjs' : 'cjs'}`,
     },
     rollupOptions: {
-      external: ['react', 'react/jsx-runtime'],
+      external: ['react', 'react/jsx-runtime', '@djeka07/utils'],
       output: {
         preserveModules: true,
         globals: {
           react: 'React',
           'react/jsx-runtime': 'react/jsx-runtime',
+          '@djeka07/utils': '@djeka07/utils',
         },
       },
     },
